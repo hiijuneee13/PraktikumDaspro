6e12f3e0ef4d79910b902d6102000a3dcd9b271e
Rizal Maulana <xjunngoat1@gmail.com>
2025-10-02 10:59:25 +0700

    Tugas 2

---
+++ Jobsheet5/Tugas2.java
@@ -0,0 +1,19 @@
+package Jobsheet5;
+import java.util.Scanner;
+
+public class Tugas2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan sebuah angka: ");
+        int angka = sc.nextInt();
+
+        if (angka % 2 == 0) {
+            System.out.println("Bilangan " + angka + " adalah GENAP.");
+        } else {
+            System.out.println("Bilangan " + angka + " adalah GANJIL.");
+        }
+
+        sc.close();
+    }
+}
