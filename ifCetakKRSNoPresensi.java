0b7474e6433ba94cd9430078c84d133f93745e0c
Rizal Maulana <xjunngoat1@gmail.com>
2025-10-02 08:42:03 +0700

    Perubahan baru

--- ifCetakKRSNoPresensi.java
+++ ifCetakKRSNoPresensi.java
@@ -15,11 +15,6 @@ public class ifCetakKRSNoPresensi {
         if (uktLunas) {
             System.out.println("Pembayaran UKT terverifikasi.");
              System.out.print("Silahkan cetak KRS dan minta tanda tangan DPA");
-        } else {
-            System.out.println("Pembayaran UKT tidak terverifikasi. Silakan lunasi UKT terlebih dahulu.");
         }
-
-        // Tutup Scanner
-        sc.close();
     }
 }
