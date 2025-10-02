4f50e736f536534c165d305061f65763e001e8ac
Rizal Maulana <xjunngoat1@gmail.com>
2025-10-02 11:33:40 +0700

    Tugas 3

---
+++ Jobsheet5/Tugas3.java
@@ -0,0 +1,32 @@
+package Jobsheet5;
+import java.util.Scanner;
+
+public class Tugas3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("=== Sistem Akses WiFi Kampus ===");
+        System.out.print("Masukkan jenis pengguna (dosen/mahasiswa/lain): ");
+        String jenisPengguna = sc.next();
+
+        if (jenisPengguna.equalsIgnoreCase("dosen")) {
+            // Jika dosen
+            System.out.println("Akses WiFi diberikan (dosen)");
+        } else if (jenisPengguna.equalsIgnoreCase("mahasiswa")) {
+            // Jika mahasiswa, cek SKS
+            System.out.print("Masukkan jumlah SKS yang diambil: ");
+            int sks = sc.nextInt();
+
+            if (sks >= 12) {
+                System.out.println("Akses WiFi diberikan (mahasiswa aktif)");
+            } else {
+                System.out.println("Akses ditolak, SKS kurang dari 12");
+            }
+        } else {
+            // Jika bukan dosen maupun mahasiswa
+            System.out.println("Akses ditolak");
+        }
+
+        sc.close();
+    }
+}
